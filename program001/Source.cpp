34ce7d3c663fdcc3ab5288a51588dc0564cf725e
ปิยชัย แก้วชุ่ม <kaonaka@hotmail.co.th>
2020-11-01 21:34:19 +0700

    fixed

--- program001/Source.cpp
+++ program001/Source.cpp
@@ -5,10 +5,12 @@ int main()
 {
 	char n[100] = " ";
 	int x, j = 0;
+	printf("Enter integer : ");
 	scanf_s("%s", n, 100);
 	x = strlen(n);
 	printf("x = %d\n", x);
 	int u = x % 3;
+	printf("You have ");
 	for (int i = 0; i <= x; i++)
 	{
 		printf("%c", n[i]);
@@ -26,5 +28,6 @@ int main()
 			u += 0;
 		}
 	}
+	printf(" Baht");
 	return 0;
 }
